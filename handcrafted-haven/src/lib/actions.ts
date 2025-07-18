d52417e2269fd968b792ce604b2d3ca69aa11e26
bcadolf <adolfbrandon96@gmail.com>
2025-07-18 16:54:56 -0400

    validation schemas moved to new file, all Create statements done

--- handcrafted-haven/src/lib/actions.ts
+++ handcrafted-haven/src/lib/actions.ts
@@ -1,20 +1,17 @@
 'use server';
 
 import postgres from 'postgres';
-import { z } from 'zod';
 import bcrypt from 'bcryptjs';
+import {
+  SellerSchema,
+  ProductSchema,
+  CartDetailSchema,
+  OrderSchema,
+  OrderItemSchema,
+  ReviewSchema,
+} from './validation/schemas';
 
 const sql = await postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
-
-const SellerSchema = z.object({
-  id: z.uuid(),
-  owner_first: z.string(),
-  owner_last: z.string(),
-  store_name: z.string(),
-  store_email: z.string(),
-  store_address: z.string(),
-  password: z.string(),
-});
 
 const CreateSeller = SellerSchema.omit({ id: true });
 
@@ -56,15 +53,6 @@ export async function createSeller(formData: FormData) {
   }
 }
 
-const ProductSchema = z.object({
-  id: z.coerce.number().int(),
-  item_name: z.string(),
-  item_price_cents: z.number().transform((val) => Math.round(val * 100)),
-  item_stock: z.number().int().min(0),
-  item_description: z.string(),
-  seller_id: z.uuid(),
-});
-
 const CreateProduct = ProductSchema.omit({ id: true });
 
 export async function createProduct(formData: FormData) {
@@ -94,21 +82,14 @@ export async function createProduct(formData: FormData) {
   try {
     await sql`
         INSERT INTO products (item_name, item_price, item_stock, item_description, seller_id)
-        VALUES (${item_name}, ${item_price_cents}, ${item_stock}, ${item_description}, ${seller_id})
+        VALUES (${item_name}, ${Math.round(
+      item_price_cents / 100
+    )}, ${item_stock}, ${item_description}, ${seller_id})
     `;
   } catch (error) {
     console.log(error);
   }
 }
-
-const UserSchema = z.object({
-  id: z.uuid(),
-  user_first: z.string(),
-  user_last: z.string(),
-  user_email: z.string(),
-  user_address: z.string(),
-  is_guest: z.boolean(),
-});
 
 export async function createUser() {
   try {
@@ -129,6 +110,137 @@ export async function cart({ user_id }: { user_id: string }) {
         `;
 
     return result[0]?.id;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const CreateCartDetail = CartDetailSchema.omit({ id: true });
+
+export async function createCartDetail(formData: FormData) {
+  const validatedData = CreateCartDetail.safeParse({
+    cart_id: formData.get('cart_id'),
+    seller_id: formData.get('seller_id'),
+    product_id: formData.get('product_id'),
+    quantity: formData.get('quantity'),
+  });
+
+  if (!validatedData.success) {
+    return {
+      errors: validatedData.error,
+      message: 'Missing or Invalid Information. Failed to Add to Cart Detail.',
+    };
+  }
+
+  const { cart_id, seller_id, product_id, quantity } = validatedData.data;
+
+  try {
+    await sql`
+    INSERT INTO cart_details (cart_id, seller_id, product_id, quantity) VALUES (${cart_id}, ${seller_id}, ${product_id}, ${quantity})
+  `;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const CreateOrder = OrderSchema.omit({ id: true });
+
+export async function createOrder(formData: FormData) {
+  const validatedData = CreateOrder.safeParse({
+    user_id: formData.get('user_id'),
+    user_name: formData.get('user_name'),
+    user_email: formData.get('user_email'),
+    user_address: formData.get('user_address'),
+    total_price_cents: formData.get('total_price'),
+  });
+
+  if (!validatedData.success) {
+    return {
+      errors: validatedData.error,
+      message: 'Missing or Invalid Information. Failed to Create Order.',
+    };
+  }
+
+  const { user_id, user_name, user_email, user_address, total_price_cents } =
+    validatedData.data;
+
+  try {
+    await sql`
+        INSERT INTO orders (user_id, user_name, user_email, user_address, total_price) VALUES (${user_id}, ${user_name}, ${user_email}, ${user_address}, ${Math.round(
+      total_price_cents / 100
+    )})
+    `;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const CreateOrderItem = OrderItemSchema.omit({ id: true });
+
+export async function createOrderItem(formData: FormData) {
+  const validatedData = CreateOrderItem.safeParse({
+    order_id: formData.get('order_id'),
+    seller_id: formData.get('seller_id'),
+    store_name: formData.get('store_name'),
+    product_id: formData.get('product_id'),
+    item_name: formData.get('item_name'),
+    item_price_cents: formData.get('item_price'),
+    quantity: formData.get('quantity'),
+  });
+
+  if (!validatedData.success) {
+    return {
+      errors: validatedData.error,
+      message: 'Missing or Invalid Information. Failed to Create Order.',
+    };
+  }
+
+  const {
+    order_id,
+    seller_id,
+    store_name,
+    product_id,
+    item_name,
+    item_price_cents,
+    quantity,
+  } = validatedData.data;
+
+  try {
+    await sql`
+        INSERT INTO order_items (order_id, seller_id, store_name, product_id, item_name, item_price, quantity) VALUES (${order_id}, ${seller_id}, ${store_name}, ${product_id}, ${item_name}, ${Math.round(
+      item_price_cents / 100
+    )}, ${quantity})
+    `;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const CreateReview = ReviewSchema.omit({ id: true });
+
+export async function createReview(formData: FormData) {
+  const validatedData = CreateReview.safeParse({
+    rating: formData.get('rating'),
+    product_id: formData.get('product_id'),
+    seller_id: formData.get('seller_id'),
+    user_name: formData.get('user_name'),
+    description: formData.get('description'),
+  });
+
+  if (!validatedData.success) {
+    return {
+      errors: validatedData.error,
+      message: 'Missing or Invalid Information. Failed to Create Order.',
+    };
+  }
+
+  const { rating, product_id, seller_id, user_name, description } =
+    validatedData.data;
+
+  try {
+    await sql`
+     INSERT INTO reviews (rating, product_id, seller_id, user_name, description) VALUES (${rating}, ${product_id}, ${seller_id}, ${user_name}, ${description})
+    `;
   } catch (error) {
     console.log(error);
   }
