445fabbbc3f72a1df401571ef15ddd02785921d2
Arthur Weale <ajayrossy340@gmail.com>
2025-07-22 18:02:16 +0200

    Configured for different urls

--- handcrafted-haven/next.config.ts
+++ handcrafted-haven/next.config.ts
@@ -5,7 +5,12 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
+        protocol: 'https',
         hostname:'images.unsplash.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
       }
     ]
   },
