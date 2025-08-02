93e3fff18b47e70134e9f925997bd20fbf11051e
Sadly4343 <loganhartshorn@gmail.com>
2025-08-02 06:15:14 -0600

    update

--- handcrafted-haven/src/app/api/products/cart/route.ts
+++ handcrafted-haven/src/app/api/products/cart/route.ts
@@ -1,11 +1,12 @@
 import { NextResponse } from "next/server";
 import { createUser, createCart, addProductToCart} from "@/lib/actions";
-import { success } from "zod";
+
 
 export async function POST(request: Request) {
     try {
         const body = await request.json();
-        let { user_id, product_id, seller_id, quantity} = body;
+        let { user_id } = body;
+        const {product_id, seller_id, quantity} = body;
 
          if (!user_id) {
         user_id = await createUser();
