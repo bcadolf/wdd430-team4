09e1ddcb8bda6bb7796f34433278a2d81ec9300e
Sadly4343 <loganhartshorn@gmail.com>
2025-08-01 18:14:04 -0600

    updates

--- handcrafted-haven/src/app/api/products/[id]/route.ts
+++ handcrafted-haven/src/app/api/products/[id]/route.ts
@@ -1,10 +1,22 @@
 import { NextResponse } from "next/server";
 import { getProductByParam } from "@/lib/data";
 
-export async function GET(_request: Request, { params }: { params: { id: string }}) {
-    const product = await getProductByParam({ field: "id", value: params.id });
-    if (!product) {
-        return NextResponse.json({ error: "Not FOund"}, { status: 404});
-    }
-    return NextResponse.json(product);
+export async function GET(
+  _request: Request,
+  context: { params: Promise<{ id: string }> }
+): Promise<NextResponse> {
+  const { id } = await context.params;
+  const numericId = Number(id);
+  if (isNaN(numericId)) {
+    return NextResponse.json({ error: "Invalid ID" }, { status: 400 });
+  }
+
+  const products = (await getProductByParam({ field: "id", value: numericId })) || [];
+  const product = products[0];
+
+  if (!product) {
+    return NextResponse.json({ error: "Not Found" }, { status: 404 });
+  }
+
+  return NextResponse.json(product);
 }
--- handcrafted-haven/src/app/products/[id]/page.tsx
+++ handcrafted-haven/src/app/products/[id]/page.tsx
@@ -1,12 +1,15 @@
 
 import { notFound } from "next/navigation";
+import Image from "next/image";
 
 import { getProductByParam } from "@/lib/data";
 
-export default async function ProductPage(props: { params: Promise<{ id: string}>}) {
-    const { id } = await props.params;
+export default async function ProductPage({params}: { params: Promise<{ id: string }>}) {
+    const { id } = await params;
+    const numberId = Number(id)
+    if (isNaN(numberId)) return notFound();
 
-    const products = await getProductByParam({ field: "id", value: id});
+    const products = await getProductByParam({ field: "id", value: numberId});
     console.log(products);
     const product = products?.[0];
 
@@ -17,7 +20,13 @@ export default async function ProductPage(props: { params: Promise<{ id: string}
 
         <div>
             <h1>{product.item_name}</h1>
-            <img src={product.item_image} alt={product.item_name} />
+            <Image
+                src={product.item_image}
+                alt={product.item_name}
+                width={400}
+                height={400}
+                style={{ objectFit: "contain"}}
+            />
             <p>{product.item_description}</p>
             <p>Price: ${product.item_price}</p>
             <p>Stock: {product.item_stock}</p>
