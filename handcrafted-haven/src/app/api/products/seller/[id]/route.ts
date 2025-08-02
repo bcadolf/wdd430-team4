5291f122b2bd8c73a834f57edcfcbac5be501a29
Sadly4343 <loganhartshorn@gmail.com>
2025-08-02 06:12:29 -0600

    updates

--- handcrafted-haven/src/app/api/products/seller/[id]/route.ts
+++ handcrafted-haven/src/app/api/products/seller/[id]/route.ts
@@ -2,13 +2,14 @@ import { NextResponse } from "next/server";
 import { getSellerByParam } from "@/lib/data";
 export const runtime = "nodejs";
 
-export async function GET(request: Request, { params}: { params: { id: string }}) {
+export async function GET(request: Request, props: { params: Promise<{ id: string }>}) {
+    const params = await props.params;
 
-const seller = await getSellerByParam({ field: "id", value: params.id });
+    const seller = await getSellerByParam({ field: "id", value: params.id });
 
     if (!seller) {
         return NextResponse.json({ error: "Seller not Valid"}, { status: 404});
     }
 
-  return NextResponse.json(seller);
+    return NextResponse.json(seller);
 }
