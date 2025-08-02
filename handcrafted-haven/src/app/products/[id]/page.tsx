b8e0bdd3118fcceefd8fdb6b8092bbaaaf99a22b
Sadly4343 <loganhartshorn@gmail.com>
2025-08-01 18:27:33 -0600

    updates

--- handcrafted-haven/src/app/products/[id]/page.tsx
+++ handcrafted-haven/src/app/products/[id]/page.tsx
@@ -1,6 +1,4 @@
 import { notFound } from 'next/navigation';
-
-import { notFound } from "next/navigation";
 import Image from "next/image";
 
 import { getProductByParam } from '@/lib/data';
@@ -9,19 +7,10 @@ export default async function ProductPage({params}: { params: Promise<{ id: stri
     const { id } = await params;
     const numberId = Number(id)
     if (isNaN(numberId)) return notFound();
-export default async function ProductPage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const { id } = params;
 
     const products = await getProductByParam({ field: "id", value: numberId});
     console.log(products);
     const product = products?.[0];
-  const products = await getProductByParam({ field: 'id', value: id });
-  console.log(products);
-  const product = products?.[0];
 
   if (!product) return notFound();
 
@@ -42,13 +31,4 @@ export default async function ProductPage({
         </div>
     )
 
-  return (
-    <div>
-      <h1>{product.item_name}</h1>
-      <img src={product.item_image} alt={product.item_name} />
-      <p>{product.item_description}</p>
-      <p>Price: ${product.item_price}</p>
-      <p>Stock: {product.item_stock}</p>
-    </div>
-  );
 }
