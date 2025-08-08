c7fa26b3b9b42391c1908be679d8643ac5a3f4e6
Arthur Weale <ajayrossy340@gmail.com>
2025-08-08 02:31:05 +0200

    Removed hero image component

--- handcrafted-haven/src/app/page.tsx
+++ handcrafted-haven/src/app/page.tsx
@@ -1,45 +1,11 @@
-'use client'
+import HeroImage from '@/components/ui/HeroImage';
+import { getDistinctCategories } from '@/lib/data';
+import CategoryButtons from '@/components/ui/CategoryButtons';
 
-import Image from "next/image"
-import { ProductCard } from "@/components/product/ProductCard";
-import { images } from '@/lib/hero';
-import { useState, useEffect} from 'react';
-
-export default function Home(){
-  const [currentImage, setCurrentImage] = useState(0)
-
-  const nextSlide = ()=> {
-    setCurrentImage((prevIndex)=>
-      prevIndex === images.length - 1 ? 0 : prevIndex + 1)
-  }
-
-  useEffect(()=>{
-    const interval = setInterval(()=>{
-      nextSlide()
-    }, 15000)
-
-    return ()=> clearInterval(interval);
-  }, [currentImage])
-
+export default async function Home(){
+  const categories = await getDistinctCategories();
   return <>
-    <div className="relative h-[500px] w-full ">
-        <div className="w-full h-10 overflow-hidden">
-          <Image
-          src={images[currentImage].url}
-          alt="Hero image of a handcrafted item"
-          fill
-          className="object-cover"
-          priority
-          />
-        </div>
-        <div className="absolute inset-0 bg-opacity-30 flex flex-col items-center justify-center text-center px-4">
-          <h1 className="text-4xl md:text-6xl font-bold text-white mb-4">Handcrafted Treasures</h1>
-          <p className="text-white text-lg md:text-xl max-w-xl">Unique pieces made with love and intention.</p>
-        </div>
-      </div>
-      <div>
-        
-      </div>
-    </>
-
+    <HeroImage/>
+    <CategoryButtons categories={categories}/>
+  </>
 }
